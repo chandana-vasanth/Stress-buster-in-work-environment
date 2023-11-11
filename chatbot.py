15309c1b5ccbf127039704b42f74ad2c98ca504c
Angelrose19 <angelroseinchodikkaran@gmail.com>
2023-11-12 03:57:37 +0530

    Chatbot

--- chatbot.py
+++ chatbot.py
@@ -1,36 +1,80 @@
 from flask import Flask, render_template, request
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
-import torch
 
 app = Flask(__name__)
 
-# Load the GPT-2 model and tokenizer
-model_name = "gpt2"
-tokenizer = GPT2Tokenizer.from_pretrained(model_name)
-model = GPT2LMHeadModel.from_pretrained(model_name)
+questions = [
+    {
+        'text': 'How stressed are you today?',
+        'options': ['Low', 'Medium', 'High'],
+        'response': {
+            'Low': "Don't be overstressed. Take a break and relax!",
+            'Medium': "It's important to manage stress. Try some deep breathing exercises.",
+            'High': "Take a deep breath. Consider talking to someone or taking a break to relax."
+        }
+    },
+    {
+        'text': 'How well did you sleep last night?',
+        'options': ['Well', 'Okay', 'Poorly'],
+        'response': {
+            'Well': "Great! A good night's sleep is important for your well-being.",
+            'Okay': "If you're having trouble sleeping, consider establishing a bedtime routine.",
+            'Poorly': "Poor sleep can impact your day. Try creating a calm bedtime environment."
+        }
+    },
+    {
+        'text': 'What is one thing that usually brings a smile to your face, even on tough days?',
+        'options': ['A funny joke', 'Watching a movie', 'Going out for a walk'],
+        'response': {
+            'A funny joke': "Okey, then let me crack a joke. Do you know Why the computer catch cold? Because it left its Windows open!",
+            'Watching a movie': "Grab some popcorn and watch Yeh jawani hai diwani",
+            'Going out for walk': "PS: Don't forget to take your pet!!"
+        }
+    },
+    {
+        'text': "If you were a superhero, what would your superpower be?",
+        'options': ['Invisibility', 'Super strength', 'Teleportation'],
+        'response': {
+            'Invisibility': "Imagine all the fun pranks you could pull or the secret missions you could embark on. What would be the first thing you'd do if you were invisible?",
+            'Super strength': "A powerhouse choice! You'd be the go-to hero for lifting cars and toppling villains",
+            'Teleportation': "Teleportation, the ultimate travel power! No more traffic or long flights."
+        }
+    },
+    {
+        'text': "If you could instantly master any skill, what would it be?",
+        'options': ['Playing a musical instrument', 'Super strength', 'Teleportation'],
+        'response': {
+            'Playing a musical instrument': "Oh great choice!",
+            'Speaking a new language': "Oh great choice!",
+            'Cooking like a chef': "Oh great choice!"
+        }
+    }
+]
 
-# Set the model to evaluation mode
-model.eval()
+current_question_index = 0
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    global current_question_index
+    current_question = questions[current_question_index]
 
-@app.route('/get_response', methods=['POST'])
-def get_response():
-    user_message = request.form['user_message']
+    if request.method == 'POST':
+        user_answer = request.form['answer']
 
-    # Encode the user message
-    input_ids = tokenizer.encode(user_message, return_tensors="pt")
+        if user_answer in current_question['response']:
+            response = current_question['response'][user_answer]
+        else:
+            response = "I didn't understand your response. Please choose a valid option."
 
-    # Generate a response from the model
-    with torch.no_grad():
-        output = model.generate(input_ids, max_length=100, num_beams=5, no_repeat_ngram_size=2, top_k=50, top_p=0.95, temperature=0.7)
+        current_question_index += 1
 
-    # Decode and extract the response
-    bot_response = tokenizer.decode(output[0], skip_special_tokens=True)
+        if current_question_index < len(questions):
+            current_question = questions[current_question_index]
+        else:
+            current_question = None
 
-    return {'bot_response': bot_response}
+        return render_template('index.html', question=current_question, response=response)
+
+    return render_template('index.html', question=current_question)
 
 if __name__ == '__main__':
     app.run(debug=True)
